a5c11e2f83849052c7352792f795d8513ea7c1aa
llxxb <llxxbb@yeah.net>
2018-09-23 10:13:26 +0800

    * use reference parameter to improve performance     StoreServiceTrait::generate_store_task .

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,19 @@ use nature_common::*;
 
 
 #[no_mangle]
-pub extern fn rtn_none(para: &CallOutParameter) -> ConverterReturned {
+pub extern fn rtn_none(_para: &CallOutParameter) -> ConverterReturned {
     ConverterReturned::None
 }
 
 #[no_mangle]
-pub extern fn rtn_one(para: &CallOutParameter) -> ConverterReturned {
+pub extern fn rtn_one(_para: &CallOutParameter) -> ConverterReturned {
     let mut instance = Instance::default();
     instance.data.content = "one".to_string();
     ConverterReturned::Instances(vec![instance])
 }
 
 #[no_mangle]
-pub extern fn rtn_tow(para: &CallOutParameter) -> ConverterReturned {
+pub extern fn rtn_tow(_para: &CallOutParameter) -> ConverterReturned {
     let mut one = Instance::default();
     one.data.content = "one".to_string();
     let mut two = Instance::default();
@@ -25,12 +25,12 @@ pub extern fn rtn_tow(para: &CallOutParameter) -> ConverterReturned {
 }
 
 #[no_mangle]
-pub extern fn rtn_logical_error(para: &CallOutParameter) -> ConverterReturned {
+pub extern fn rtn_logical_error(_para: &CallOutParameter) -> ConverterReturned {
     ConverterReturned::LogicalError("logical".to_string())
 }
 
 #[no_mangle]
-pub extern fn rtn_environment_error(para: &CallOutParameter) -> ConverterReturned {
+pub extern fn rtn_environment_error(_para: &CallOutParameter) -> ConverterReturned {
     ConverterReturned::EnvError
 }
 
