cd81b31981bdbe58d123dff7d80f2b880277c118
llxxbb <llxxbb@yeah.net>
2019-05-04 09:12:03 +0800

    fix sqlite test problems

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,19 @@ extern crate nature_common;
 use nature_common::*;
 
 #[no_mangle]
-#[deny(unused_attributes)]
+#[allow(unused_attributes)]
 pub extern fn rtn_none(_para: &CallOutParameter) -> ConverterReturned {
     ConverterReturned::None
 }
 
 #[no_mangle]
-#[deny(unused_attributes)]
+#[allow(unused_attributes)]
 pub extern fn rtn_logical_error(_para: &CallOutParameter) -> ConverterReturned {
     ConverterReturned::LogicalError("logical".to_string())
 }
 
 #[no_mangle]
-#[deny(unused_attributes)]
+#[allow(unused_attributes)]
 pub extern fn rtn_one(_para: &CallOutParameter) -> ConverterReturned {
     let mut instance = Instance::default();
     instance.data.content = "one".to_string();
@@ -23,7 +23,7 @@ pub extern fn rtn_one(_para: &CallOutParameter) -> ConverterReturned {
 }
 
 #[no_mangle]
-#[deny(unused_attributes)]
+#[allow(unused_attributes)]
 pub extern fn rtn_tow(_para: &CallOutParameter) -> ConverterReturned {
     let mut one = Instance::default();
     one.data.content = "one".to_string();
@@ -33,7 +33,7 @@ pub extern fn rtn_tow(_para: &CallOutParameter) -> ConverterReturned {
 }
 
 #[no_mangle]
-#[deny(unused_attributes)]
+#[allow(unused_attributes)]
 pub extern fn rtn_environment_error(_para: &CallOutParameter) -> ConverterReturned {
     ConverterReturned::EnvError
 }
