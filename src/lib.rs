f7427d968a84f5e62cc68f262aafcb0c9917739f
llxxbb <llxxbb@yeah.net>
2019-11-10 19:21:16 +0800

    fix warn and update demo doc

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,21 @@ use nature_common::*;
 
 #[no_mangle]
 #[allow(unused_attributes)]
+#[allow(improper_ctypes)]
 pub extern fn rtn_none(_para: &ConverterParameter) -> ConverterReturned {
     ConverterReturned::None
 }
 
 #[no_mangle]
 #[allow(unused_attributes)]
+#[allow(improper_ctypes)]
 pub extern fn rtn_logical_error(_para: &ConverterParameter) -> ConverterReturned {
     ConverterReturned::LogicalError("logical".to_string())
 }
 
 #[no_mangle]
 #[allow(unused_attributes)]
+#[allow(improper_ctypes)]
 pub extern fn rtn_one(_para: &ConverterParameter) -> ConverterReturned {
     let mut instance = Instance::default();
     instance.data.content = "one".to_string();
@@ -24,6 +27,7 @@ pub extern fn rtn_one(_para: &ConverterParameter) -> ConverterReturned {
 
 #[no_mangle]
 #[allow(unused_attributes)]
+#[allow(improper_ctypes)]
 pub extern fn rtn_tow(_para: &ConverterParameter) -> ConverterReturned {
     let mut one = Instance::default();
     one.data.content = "one".to_string();
@@ -34,6 +38,7 @@ pub extern fn rtn_tow(_para: &ConverterParameter) -> ConverterReturned {
 
 #[no_mangle]
 #[allow(unused_attributes)]
+#[allow(improper_ctypes)]
 pub extern fn rtn_environment_error(_para: &ConverterParameter) -> ConverterReturned {
     ConverterReturned::EnvError
 }
